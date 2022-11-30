3d23fe623fdb63ad6c5e22ed93cc45cc9e31c2a9
EvaLeona <115712998+EvaLeona@users.noreply.github.com>
2022-11-30 20:22:13 +0300

    Update zvezdochka.cs

--- zvezdochka.cs
+++ zvezdochka.cs
@@ -1,17 +1,16 @@
-﻿/* 
-Задача со звездочкой: Написать программу для перевода римских чисел в десятичные арабские.
+
 III -> 3
 LVIII -> 58
 MCMXCIV -> 1994
 */
 
 int RomeToAracbic(char sumbol)
-{   //возвращает арабское число соответствующее входной римской цифре
+{   
     string romeNumeric = "IVXLCDM";
     int[] arabicNumeric = new int[7] { 1, 5, 10, 50, 100, 500, 1000 };
     int index = romeNumeric.IndexOf(sumbol);
     if (index >= 0) return arabicNumeric[index];
-    return 0;//если не найден символ римских цифр возвращаем 0
+    return 0;
 }
 
 int RomeNumberParse(string romeNumber)
@@ -21,12 +20,12 @@ int RomeNumberParse(string romeNumber)
     {
         int currentNumber = RomeToAracbic(romeNumber[i]);
         if (currentNumber == 0)
-        {   //выбрасываем исключение если обнаружен неверный символ
+        {   
             throw new Exception("Invalid character in the Roman notation of the number.");
         }
         if (i + 1 < romeNumber.Length)
         {
-            //если есть следующая цифра
+            /
             if (currentNumber < RomeToAracbic(romeNumber[i + 1]))
             {
                 result -= currentNumber;
@@ -37,7 +36,7 @@ int RomeNumberParse(string romeNumber)
             }
         }
         else
-        // обработка последней цифры 
+        
         {
             result += currentNumber;
         }
@@ -45,7 +44,7 @@ int RomeNumberParse(string romeNumber)
     return result;
 }
 
-//проверка по значениям из задания
+
 string[] exampleRome = { "III", "LVIII", "MCMXCIV" };
 foreach (var item in exampleRome)
 {
@@ -53,8 +52,3 @@ foreach (var item in exampleRome)
     Console.WriteLine($"{item} = {arabicNumber}");
 }
 
-/* вывод консоли
-III = 3
-LVIII = 58
-MCMXCIV = 1994
-*/
