547f5b9a744dabd6cffe4651b7ff5880429d1bbf
Eva <eva.leona@mail.ru>
2022-11-30 20:20:26 +0300

    50

---
+++ 50.cs
@@ -0,0 +1,34 @@
+﻿
+bool SearchNumberInArray(int[,] array, int findNumber)
+{
+
+    bool isFound = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == findNumber)
+            {
+                Console.Write((isFound ? "" : "Найдено:\n"));
+                Console.WriteLine($"[{i},{j}]");
+                isFound = true;
+            }
+        }
+    }
+    return isFound;
+}
+
+
+
+
+int[,] myTestArray = { { 1, 4, 7, 2 },
+                       { 5, 9, 2, 3 },
+                       { 8, 4, 2, 4}};
+
+Console.WriteLine("Введите число для поиска: ");
+int findNum = int.Parse(Console.ReadLine());
+bool resultSearch = SearchNumberInArray(myTestArray, findNum);
+if (!resultSearch)
+{
+    Console.WriteLine($"Элемент со значением {findNum} не найдены");
+}
